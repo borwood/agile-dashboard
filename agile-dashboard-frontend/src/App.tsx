c5c5f34b3d33330cb77dbbe92efab20d44588259
Brennan Norwood <brennanjnorwood@gmail.com>
2025-04-15 22:04:35 -0400

    Adding tailwind 4 to frontend

--- agile-dashboard-frontend/src/App.tsx
+++ agile-dashboard-frontend/src/App.tsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
+      <div className="centered flex flex-col items-center justify-center min-h-screen bg-quaternary">
+        <h1 className="text-2xl rounded-2xl bg-secondary p-2 text-avocado-500">
+          Agile Dashboard
+        </h1>
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
